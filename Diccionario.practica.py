268d82acee4360339551a752f92c42e1dc627c6d
Andres <anderdavidrojasvalencia@gmail.com>
2025-05-31 16:29:47 -0500

    ejemplo de if

---
+++ Diccionario.practica.py
@@ -0,0 +1,27 @@
+product1=input("ingrese el primer producto: ")
+product2=input("ingrese el segundo producto: ")
+product3=input("ingrese el tercer producto: ")
+
+preciounita1=float(input("ingrese el precio del primer producto"))
+preciounita2=float(input("ingrese el precio del segundo producto"))
+preciounita3=float(input("ingrese el precio del tercer producto"))
+
+cantidad1=int(input("ingrese la cantidad del primer producto"))
+cantidad2=int(input("ingrese la cantidad del segundo producto"))
+cantidad3=int(input("ingrese la cantidad de el tercer producto"))
+
+tup1=(product1,preciounita1)
+tup2=(product2,preciounita2)
+tup3=(product3,preciounita3)
+
+lista1=[tup1,cantidad1]
+lista2=[tup2,cantidad2]
+lista3=[tup3,cantidad3]
+
+informacion={"product1":product1,
+             "product2":product2,
+             "product3":product3}
+
+oper1=
+oper2=
+oper3=
---
+++ sentencia.if.py
@@ -0,0 +1,4 @@
+edad=30
+
+if edad>=18:
+    print("es un adulto")
