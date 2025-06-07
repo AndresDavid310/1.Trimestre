7eee7fce3efbcd8d596d26419f8e83219355f158
Andres <anderdavidrojasvalencia@gmail.com>
2025-06-07 17:44:25 -0500

    ejercicios

---
+++ Ejercicios.condicionales.py
@@ -0,0 +1,168 @@
+# print("condicionales y operaciones")
+# print("ejercicio1")
+
+# num=int(input("ingrese un numero por favor: "))
+
+# if num <0: 
+#     print("es negativo")
+    
+# elif num >0:
+#     print("es positivo")
+    
+# else:
+#     print("ingrese de nuevo el numero")
+
+# print("ejercicio2")
+
+# num1=int(input("ingrese un numero: "))
+# num2=int(input("ingrese otro por favor: "))
+
+# if num1 <num2:
+#     print ("es menor que el 2 numero")
+    
+# elif num2 >num1:
+#     print("es meyor que el primer numero")
+    
+# else:
+#     ("ingrese de nuevo el numero")
+
+# print("ejercicio3")
+
+# num3=int(input("ingrese un numero: "))
+
+# if num3 %2==0:
+#     print("es par")
+    
+# elif num3 %2==1:
+#     print(" es impar")
+    
+# else:
+#     print("ingrese de nuevo el numero")
+
+
+# print("Ejercicio4")
+
+# num4=int(input("ingrese un numero: "))
+
+# if num4 <=9:
+#     print("no se encuentra del 10 al 20")
+    
+# elif num4 >=10:
+#     print("si se encuentra entre del 10 al 20")
+    
+# elif num4 >=21:
+#     print("no se encuentra de el 10 al 20")
+    
+# else:
+#     print("ingrese de nuevo el numero")
+
+
+# print("Ejercicio5")
+
+# num5=int(input("ingrese un numero: "))
+# num55=int(input("ingrese un segundo numero: "))
+# num555=int(input("ingrese un tercer numero: "))
+
+# if num5 >= num55 and num5 >= num555:
+#     print("es mayor",num5)
+    
+# elif num55 >=num5 and num55 >= num555:
+#     print("es mayor",num55)
+    
+# else:
+#     print("es mayor el",num555)
+
+
+# print("Ejercicio6")
+
+# precio=float(input("ingrese el precio final: "))
+
+# if precio >100:
+#     descuento= precio*0.10
+#     preciofinal=precio-descuento
+#     print("el totals supera los 100")
+#     print(preciofinal)
+# else:
+#     print("no tiene descuento")
+
+
+# print("Ejercicio7")
+
+# edad=int(input("ingrese su edad: "))
+
+# if edad >=18:
+#     print("puedes votar")
+    
+# else:
+#     print("no tienes la edad para votar")
+
+
+# print("Ejercicio8")
+
+# tipo=input("que tipo de cliente es vip o normal: ")
+# preciototal=int(input("ingrese el precio total:"))
+
+# if tipo == "vip":
+#     descuento= preciototal*0.20
+#     preciofinal= preciototal-descuento
+#     print("el precio es de",preciofinal)
+# else:
+#     print("no tienes descuento")
+
+
+# print("Ejercicio9")
+
+# num6=int(input("ingrese un numero: "))
+
+# if num6 % 5==0 and num6 % 3==0:
+#     print("el numero es de 5 y de 3")
+    
+# else:
+#     print("no es multiplo de 5 y de 3")
+
+
+# print("Ejercicio10")
+
+# num7=int(input("ingrese un numero: "))
+
+# div1=int(input("ingrese un numero: "))
+# div2=int(input("ingrese otro numero: "))
+
+# if num7 % div1 == 0 and num7 % div2 ==0:
+#     print("el numero",num7 ,"es divisible entre" ,div1 ,"y", div2 )
+    
+# else:
+#     print("no es divisible para los 2 numeros")
+
+
+# print("listan con condicionales")
+# print("Ejercicio11")
+
+# lista=[]
+
+# numer1=int(input("ingrese un numero: "))
+# numer2=int(input("ingrese un numero: "))
+# numer3=int(input("ingrese un numero: "))
+# numer4=int(input("ingrese un numero: "))
+# numer5=int(input("ingrese un numero: "))
+
+# lista=[numer1,numer2,numer3,numer4,numer5]
+
+# if lista [2]>10:
+#     print("mayor que 10")
+    
+# else:
+#     print("es menor que 10")
+
+
+# print("Ejercicio12")
+
+# lista=[3,5,7,9]
+
+# numer8=int(input("ingrese un numero: "))
+
+# if numer8==7: 
+#     print("adivinaste el numero")
+    
+# else:
+#     print("no esta en lista")
