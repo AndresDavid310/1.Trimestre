26ea83d967f4180b913632c42ddc68bc6697cdd8
Andres David Valencia Rojas <anderdavidrojasvalencia@gmail.com>
2025-06-11 17:14:25 -0600

    Andres David Valencia

--- Ejercicios.condicionales.py
+++ Ejercicios.condicionales.py
@@ -168,4 +168,166 @@
 #     print("no esta en lista")
 
 
-print("Ejercicio13")
+# print("Ejercicio13")
+
+# lista=[4,6,2,8]
+
+# if lista[0] + lista[1] >10:
+#     print("la suma es alta")
+
+# elif lista[2]+lista[3] >10:
+#     print("la suma es menor")
+
+# elif lista[0] + lista[1] ==10: 
+#     print("la suma es igual a 10")
+    
+# else:
+#     print("la suma es menor o igual a 10")
+    
+    
+# print("EJERCICIO14")
+
+# lista=["Ana","luis","Pedro","Marta"]
+
+# nom=lista[3]
+
+# if nom == "Marta":
+#     print("el nombre es Marta")
+    
+# else:
+#     print("es otro nombre")
+    
+    
+# print("Ejercicio15")
+
+# color1=input("ingrese un color: ")
+# color2=input("ingrese un color: ")
+# color3=input("ingrese un color: ")
+
+# lista=[color1,color2,color3]
+
+# if lista[1]== "azul":
+#     lista[1]="naranja"
+#     print("el color numero 2 ha sido cambiado a", lista[1],"la lista actualizada:", lista)
+
+# else:
+#     print("la lista esta asi:", lista)
+
+
+
+# print("Tuplas")
+# print("Ejercicio16")
+
+
+# tupla=(5, 8, 12, 20)
+
+# if tupla[0]< tupla[-1]:
+#     print("orden ascendente")
+    
+# else:
+#     print("orden descendente")
+    
+    
+# print("Ejercicio17")
+
+# tupla2=(25, 32, 28)
+
+# if tupla2[1] >30:
+#     print("Edad mayor de 30")
+    
+# else:
+#     print("edad menor o igual al 30")
+    
+    
+# print("Ejercicio18")
+
+# tupla3=(1, 2, 3)
+
+# lista1=list(tupla3)
+
+# if lista1[1] == 2:
+#     lista1[1] = 10
+#     tupla3=tuple(lista1)
+#     print("la tupla actualizada queda de la siguiente manera:", tupla3,"cambiando el 2 por un 10")
+# else:
+#     print("lista no necesita actualizacion")
+
+
+# print("Ejercicio19")
+
+# tupla4=(4, 5)
+
+
+# usuario2= {"nombre": "Lucía", "edad": 20}
+
+# if usuario2["edad"]>18:
+#     usuario2["edad"]=21
+#     print("valor de la edad cambiado a 21",usuario2)
+    
+    
+# else:
+#     print(usuario2)
+    
+    
+# print("Ejercicio23")
+
+# diccionario={"nombre": "Carlos"}
+
+# if "ciudad" not in diccionario:
+#     diccionario["ciudad"] = "Bogotá"
+
+# print(diccionario)
+
+
+# print("Ejercicio24")
+
+# diccionario2= {"producto": "pan", "precio": 1200}
+
+# if "precio" in diccionario2:
+# if tupla4[1] >5:
+#     print("coordenada alta")
+    
+# else:
+#     print("coordenada baja")
+    
+    
+# print("Ejercicio20")
+
+
+# tupla1 = (3, 4)
+# tupla2 = (3, 5)
+
+# if tupla1 == tupla2:
+#     print("Tuplas iguales")
+# else:
+#     print("Tuplas diferentes")
+
+
+# print("diccionarios")
+# print("Ejercicio21")
+
+# usuario={"nombre": "juan","edad":17}
+
+# if usuario["edad"]>=18:
+#     print("Adulto")
+    
+# else:
+#     print("menor de edad")
+    
+    
+# print("Ejercicio22")
+#     print("el precio es de", diccionario2["precio"])
+    
+# else:
+#     print("no hay disponible")
+
+
+# print("Ejercicio25")
+
+# diccionario3={"pan": 1200, "leche": 2000}
+
+# if "pan" in diccionario3:
+#     print("el precio del pan es de", diccionario3["pan"])
+    
+# else:
+#     print("producto no disponible")
