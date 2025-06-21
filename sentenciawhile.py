7ba95c2f67362206dbeccfaf722d9808f7f3c7c8
Andres <anderdavidrojasvalencia@gmail.com>
2025-06-21 16:11:23 -0500

    25 ejercicios

--- sentenciawhile.py
+++ sentenciawhile.py
@@ -3,3 +3,18 @@ contador = 1
 while contador <= 100:
     print("contador:",contador)
     contador += 1
+
+
+contador1=int(input("ingrese un numero: "))
+
+while contador1 <0:
+    print(f"contador1:,{contador}")
+    contador -= 1
+    print("termino el contador")
+
+
+while true:
+    texto=input("escribe algo(o escribe ´salir´ para terminar):")
+    if texto== "salir":
+        break
+    print("escribiste:",texto)
