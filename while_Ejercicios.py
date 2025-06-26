c2237384c9a34945728a042af8ffc17a582e485f
Andrés Felipe Moreno Domínguez <andresdevbod@gmail.com>
2025-06-26 17:44:27 -0500

    while

---
+++ while_Ejercicios.py
@@ -0,0 +1,50 @@
+# print("Ejercicio1")
+
+# total=0
+# num1=int(input("ingrese un numero(0 para terminar) "))
+# while num1 != 0 :
+#     total += num1
+#     num1 =int(input("ingrese otro numero(0 para terminar): "))
+    
+# print("la suma total es:",total)
+
+# print("Ejercicio2")
+
+# contraseña=input("Escribe la contraseña:")
+
+# while contraseña !="andres123":
+#     print("contraseña incorrecta")
+#     contraseña=input("intente de nuevo: ")
+# print("Acceso concedido")
+
+
+# print("Ejercico3")
+
+# lista=[]
+
+# pro=input("ingrese un producto: ")
+
+# while pro.lower() !="fin":
+#     lista.append(pro)
+#     producto=input("ingrese el seguiente producto: ")
+#     pro=producto
+# print("lista de productos:", lista)
+
+
+print("Ejercicio4")
+
+contador=0
+pares=0
+impares=0
+
+while contador < 10:
+    num = int(input("ingrese un numero: "))
+    if num % 2 == 0:
+        pares += 1
+    else:
+        impares += 1
+    contador += 1
+print(f"pares: {pares} impares: {impares}")
+
+print("Ejercicio5")
+
